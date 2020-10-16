e4ba50ac782c0d00110cafdc3931976bbbe51198
vinay7892 <72770107+vinay7892@users.noreply.github.com>
2020-10-16 16:40:40 +0530

    my third commit

---
+++ main.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $(".effect").click(function(){
+   $("ul li a").show();
+   $(".effect").hide();
+
+  
+   
+   
+
+        
+
+    })
+})
+$(".explore").click(function(){
+    $("p").show();
+})
+
